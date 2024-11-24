b6184018fa9a6b731262f10f58287466c467bae7
Vaesea <159406785+Vaesea@users.noreply.github.com>
2024-11-24 01:00:00 +0000

    WHY
    WHY

--- src/badguy/flyingpipe.hpp
+++ src/badguy/flyingpipe.hpp
@@ -32,7 +32,7 @@ public:
   virtual std::string get_class_name() const override { return class_name(); }
   static std::string display_name() { return _("Flying Pipe"); }
   virtual std::string get_display_name() const override { return display_name(); }
-  virtual GameObjectClasses get_class_types() const override { return BadGuy::get_class_types().add(typeid(FlyIngPipe)); }
+  virtual GameObjectClasses get_class_types() const override { return BadGuy::get_class_types().add(typeid(FlyingPipe)); }
   virtual std::string get_overlay_size() const override { return "3x3"; }
   virtual bool is_snipable() const override { return true; }
   virtual void freeze() override;
